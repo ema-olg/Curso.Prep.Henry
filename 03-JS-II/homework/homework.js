8417148fa6043d65c8d6a3bd83f4d699395bf804
ema-olg <76265739+ema-olg@users.noreply.github.com>
2021-01-02 18:46:49 -0300

    03-JS-II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -1,19 +1,22 @@
 // No cambies los nombres de las funciones.
 
 function obtenerMayor(x, y) {
-  // "x" e "y" son números enteros (int).
-  // Devuelve el número más grande
-  // Si son iguales, devuelve cualquiera de los dos
-  // Tu código:
+    // "x" e "y" son números enteros (int).
+    // Devuelve el número más grande
+    // Si son iguales, devuelve cualquiera de los dos
+    // Tu código:
+    if (x > y)
+        return x
+    else return y
 }
 
 function saludo(idioma) {
-  // Devuelve un saludo en tres diferentes lenguajes:
-  // Si "idioma" es "aleman", devuelve "Guten Tag!"
-  // Si "idioma" es "mandarin", devuelve "Ni Hao!"
-  // Si "idioma" es "ingles", devuelve "Hello!"
-  // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
-  // Tu código:
+    // Devuelve un saludo en tres diferentes lenguajes:
+    // Si "idioma" es "aleman", devuelve "Guten Tag!"
+    // Si "idioma" es "mandarin", devuelve "Ni Hao!"
+    // Si "idioma" es "ingles", devuelve "Hello!"
+    // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
+    // Tu código:
 }
 
 function esDiezOCinco(numero) {
@@ -23,34 +26,34 @@ function esDiezOCinco(numero) {
 }
 
 function estaEnRango(numero) {
-  // Devuelve "true" si "numero" es menor que 50 y mayor que 20
-  // De lo contrario, devuelve "false"
-  // Tu código:
+    // Devuelve "true" si "numero" es menor que 50 y mayor que 20
+    // De lo contrario, devuelve "false"
+    // Tu código:
 }
 
 function esEntero(numero) {
-  // Devuelve "true" si "numero" es un entero (int/integer)
-  // Ejemplo: 0.8 -> false
-  // Ejemplo: 1 -> true
-  // Ejemplo: -10 -> true
-  // De lo contrario, devuelve "false"
-  // Pista: Puedes resolver esto usando `Math.floor`
-  // Tu código:
+    // Devuelve "true" si "numero" es un entero (int/integer)
+    // Ejemplo: 0.8 -> false
+    // Ejemplo: 1 -> true
+    // Ejemplo: -10 -> true
+    // De lo contrario, devuelve "false"
+    // Pista: Puedes resolver esto usando `Math.floor`
+    // Tu código:
 }
 
 function fizzBuzz(numero) {
-  // Si "numero" es divisible entre 3, devuelve "fizz"
-  // Si "numero" es divisible entre 5, devuelve "buzz"
-  // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
-  // De lo contrario, devuelve el numero
+    // Si "numero" es divisible entre 3, devuelve "fizz"
+    // Si "numero" es divisible entre 5, devuelve "buzz"
+    // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
+    // De lo contrario, devuelve el numero
 }
 
 function esPrimo(numero) {
-  // Devuelve "true" si "numero" es primo
-  // De lo contrario devuelve "falso"
-  // Pista: un número primo solo es divisible por sí mismo y por 1
-  // Pista 2: Puedes resolverlo usando un bucle `for`
-  // Nota: Los números 0 y 1 NO son considerados números primos
+    // Devuelve "true" si "numero" es primo
+    // De lo contrario devuelve "falso"
+    // Pista: un número primo solo es divisible por sí mismo y por 1
+    // Pista 2: Puedes resolverlo usando un bucle `for`
+    // Nota: Los números 0 y 1 NO son considerados números primos
 }
 
 
@@ -59,11 +62,11 @@ function esPrimo(numero) {
 // --------------------------------
 
 module.exports = {
-  obtenerMayor,
-  saludo,
-  esDiezOCinco,
-  estaEnRango,
-  esEntero,
-  fizzBuzz,
-  esPrimo
+    obtenerMayor,
+    saludo,
+    esDiezOCinco,
+    estaEnRango,
+    esEntero,
+    fizzBuzz,
+    esPrimo
 };

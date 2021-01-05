d3373ba35195004b49c3237fa3a2d39b0971b5a3
ema-olg <76265739+ema-olg@users.noreply.github.com>
2021-01-04 21:26:54 -0300

    03-JS-II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -17,21 +17,28 @@ function saludo(idioma) {
     // Si "idioma" es "ingles", devuelve "Hello!"
     // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
     // Tu código:
-    if s wldchqlberbcbfqbridjcnqkjewn
-    kcmqñcmqc
-    vqkmviqoimq2
+    if (idioma === "aleman") {
+        return "Guten Tag!"
+    } else if (idioma === "mandarin") {
+        return "Ni Hao!"
+    } else if (idioma === "ingles") {
+        return "Hello!"
+    } else return "hola!"
+
 }
 
 function esDiezOCinco(numero) {
     // Devuelve "true" si "numero" es 10 o 5
     // De lo contrario, devuelve "false"
     // Tu código:
+    return numero === 5 || numero === 10
 }
 
 function estaEnRango(numero) {
     // Devuelve "true" si "numero" es menor que 50 y mayor que 20
     // De lo contrario, devuelve "false"
     // Tu código:
+    return numero < 50 && numero > 20
 }
 
 function esEntero(numero) {
@@ -42,6 +49,7 @@ function esEntero(numero) {
     // De lo contrario, devuelve "false"
     // Pista: Puedes resolver esto usando `Math.floor`
     // Tu código:
+
 }
 
 function fizzBuzz(numero) {
@@ -49,6 +57,13 @@ function fizzBuzz(numero) {
     // Si "numero" es divisible entre 5, devuelve "buzz"
     // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
     // De lo contrario, devuelve el numero
+    if (numero % 3 === 0 && numero % 5 === 0) {
+        return "fizzbuzz"
+    } else if (numero % 3 === 0) {
+        return "fizz"
+    } else if (numero % 5 === 0) {
+        return "buzz"
+    } else return numero
 }
 
 function esPrimo(numero) {
@@ -57,6 +72,7 @@ function esPrimo(numero) {
     // Pista: un número primo solo es divisible por sí mismo y por 1
     // Pista 2: Puedes resolverlo usando un bucle `for`
     // Nota: Los números 0 y 1 NO son considerados números primos
+    for (numero = )
 }
 
 
